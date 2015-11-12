51f9bc35cb55560c7012863d29cf09cf8d51bed3
Denis <denis.olek@gmail.com>
2015-11-13 00:04:56 +0100

    Added console and main menu.

---
+++ Assets/Scripts/OutputMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OutputMove : MonoBehaviour {
+
+    Text score;
+    float myTimer = 0.2f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = GetComponent<Text>();
+
+        if (score.text == "up" || score.text == "down" || score.text == "right" || score.text == "left" || score.text == "north east" || score.text == "south east" || score.text == "south west" || score.text == "north west")
+        {
+            if (myTimer > 0)
+                myTimer -= Time.deltaTime;
+
+            if (myTimer <= 0)
+            {
+                score.text = "";
+                myTimer = 0.2f;
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIControl : MonoBehaviour {
+
+    public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
